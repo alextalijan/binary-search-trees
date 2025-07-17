bc80311b967b48cd3b67cc16059d37e6bf44d794
alextalijan <acatalijan@icloud.com>
2025-07-17 17:21:02 +0200

    feat: write logic for generating BTS from array

--- js/Tree.js
+++ js/Tree.js
@@ -5,13 +5,29 @@ class Tree {
     this.root = this.buildTree(array);
   }
 
-  buildTree(array) {}
+  buildTree(array) {
+    const sortedArray = this.sortArray(array);
+
+    return this.sortedArrayToBST(sortedArray, 0, sortedArray.length - 1);
+  }
 
   sortArray(array) {
     array.sort((a, b) => a - b);
-    const noDuplicateArray = new Set(array);
+    const noDuplicatesArray = new Set(array);
 
-    return noDuplicateArray;
+    return noDuplicatesArray;
+  }
+
+  sortedArrayToBST(array, startIndex, endIndex) {
+    if (startIndex > endIndex) return null;
+
+    const midIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+
+    const rootNode = new Node(array[midIndex]);
+    rootNode.left = this.sortedArrayToBST(startIndex, midIndex - 1);
+    rootNode.right = this.sortedArrayToBST(midIndex + 1, endIndex);
+
+    return rootNode;
   }
 }
 
